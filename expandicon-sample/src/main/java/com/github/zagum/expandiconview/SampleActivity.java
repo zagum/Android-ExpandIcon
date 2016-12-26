e1dddf05c607eaa2f9032006054ebb0a41b89c2e
zagum <evgenii.zagumennyi@aviasales.ru>
2016-12-26 14:06:05 +0300

    updated sample project

--- expandicon-sample/src/main/java/com/github/zagum/expandiconview/SampleActivity.java
+++ expandicon-sample/src/main/java/com/github/zagum/expandiconview/SampleActivity.java
@@ -10,21 +10,39 @@ import com.github.zagum.expandicon.ExpandIconView;
 
 public class SampleActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
-  private ExpandIconView expandIconView;
+  private ExpandIconView expandIconView1;
+  private ExpandIconView expandIconView2;
+  private ExpandIconView expandIconView3;
   private View container;
   private GestureDetector gestureDetector;
   private View swipeDetectionView;
+  private View click;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_sample);
-    expandIconView = (ExpandIconView) findViewById(R.id.expand_icon);
+
+    expandIconView1 = (ExpandIconView) findViewById(R.id.expand_icon1);
+    expandIconView2 = (ExpandIconView) findViewById(R.id.expand_icon2);
+    expandIconView3 = (ExpandIconView) findViewById(R.id.expand_icon3);
+
+    expandIconView1.setFraction(.5f, false);
+    expandIconView2.setFraction(.5f, false);
+
     container = findViewById(R.id.container);
     container.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        expandIconView.switchState();
+        expandIconView1.switchState();
+      }
+    });
+
+    click = findViewById(R.id.click);
+    click.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        expandIconView3.switchState();
       }
     });
 
@@ -40,6 +58,10 @@ public class SampleActivity extends AppCompatActivity implements GestureDetector
     swipeDetectionView.setOnTouchListener(new View.OnTouchListener() {
       @Override
       public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+          expandIconView1.setFraction(.5f, true);
+          expandIconView2.setFraction(.5f, true);
+        }
         return gestureDetector.onTouchEvent(event);
       }
     });
@@ -62,7 +84,15 @@ public class SampleActivity extends AppCompatActivity implements GestureDetector
 
   @Override
   public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-    Log.d("speed", distanceY+"");
+    Log.d("speed", distanceY + "");
+    float fraction;
+    if (Math.signum(distanceY) > 0) {
+      fraction = 1f;
+    } else {
+      fraction = 0f;
+    }
+    expandIconView1.setFraction(fraction, true);
+    expandIconView2.setFraction(fraction, true);
     return false;
   }
 
