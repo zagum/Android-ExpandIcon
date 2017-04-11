eeea9a10e4f8b18d29e7d7cf30dcfde78f889945
zagum <evgenii.zagumennyi@aviasales.ru>
2017-04-11 13:03:43 +0300

    sample updated

--- expandicon-sample/src/main/java/com/github/zagum/expandicon/sample/SampleActivity.java
+++ expandicon-sample/src/main/java/com/github/zagum/expandicon/sample/SampleActivity.java
@@ -5,89 +5,98 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+
 import com.github.zagum.expandicon.ExpandIconView;
 
 public class SampleActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
-  private ExpandIconView expandIconView1;
-  private ExpandIconView expandIconView2;
-  private ExpandIconView expandIconView3;
-  private GestureDetector gestureDetector;
-  private View swipeDetectionView;
-  private View clickView;
+	private ExpandIconView expandIconView1;
+	private ExpandIconView expandIconView2;
+	private ExpandIconView expandIconView3;
+	private ExpandIconView expandIconView4;
+	private GestureDetector gestureDetector;
+	private View swipeDetectionView;
+	private View clickView;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_sample);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sample);
 
-    expandIconView1 = (ExpandIconView) findViewById(R.id.expand_icon1);
-    expandIconView2 = (ExpandIconView) findViewById(R.id.expand_icon2);
-    expandIconView3 = (ExpandIconView) findViewById(R.id.expand_icon3);
+		expandIconView1 = (ExpandIconView) findViewById(R.id.expand_icon1);
+		expandIconView2 = (ExpandIconView) findViewById(R.id.expand_icon2);
+		expandIconView3 = (ExpandIconView) findViewById(R.id.expand_icon3);
+		expandIconView4 = (ExpandIconView) findViewById(R.id.expand_icon4);
 
-    expandIconView1.setFraction(.5f, false);
-    expandIconView2.setFraction(.5f, false);
+		expandIconView1.setFraction(.5f, false);
+		expandIconView2.setFraction(.5f, false);
 
-    clickView = findViewById(R.id.click);
-    clickView.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        expandIconView3.switchState();
-      }
-    });
+		expandIconView3.setAnimationDuration(2000);
 
-    setUpSlidingContainer();
-  }
+		clickView = findViewById(R.id.click);
+		clickView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				expandIconView3.switchState();
+			}
+		});
 
-  private void setUpSlidingContainer() {
-    gestureDetector = new GestureDetector(this, this);
-    gestureDetector.setIsLongpressEnabled(false);
+		setUpSlidingContainer();
+	}
 
-    swipeDetectionView = findViewById(R.id.swipe_detector);
-    swipeDetectionView.setClickable(true);
-    swipeDetectionView.setOnTouchListener(new View.OnTouchListener() {
-      @Override
-      public boolean onTouch(View v, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_UP) {
-          expandIconView1.setFraction(.5f, true);
-          expandIconView2.setFraction(.5f, true);
-        }
-        return gestureDetector.onTouchEvent(event);
-      }
-    });
-  }
+	private void setUpSlidingContainer() {
+		gestureDetector = new GestureDetector(this, this);
+		gestureDetector.setIsLongpressEnabled(false);
 
-  @Override
-  public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-    float fraction;
-    if (Math.signum(distanceY) > 0) {
-      fraction = 1f;
-    } else {
-      fraction = 0f;
-    }
-    expandIconView1.setFraction(fraction, true);
-    expandIconView2.setFraction(fraction, true);
-    return false;
-  }
+		swipeDetectionView = findViewById(R.id.swipe_detector);
+		swipeDetectionView.setClickable(true);
+		swipeDetectionView.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					expandIconView1.setFraction(.5f, true);
+					expandIconView2.setFraction(.5f, true);
+					expandIconView4.setFraction(.5f, true);
+				}
+				return gestureDetector.onTouchEvent(event);
+			}
+		});
+	}
 
-  @Override
-  public boolean onDown(MotionEvent e) {
-    return false;
-  }
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+		float fraction;
+		if (Math.signum(distanceY) > 0) {
+			fraction = 1f;
+		} else {
+			fraction = 0f;
+		}
+		expandIconView1.setFraction(fraction, true);
+		expandIconView2.setFraction(fraction, true);
+		expandIconView4.setFraction(fraction, true);
+		return false;
+	}
 
-  @Override
-  public boolean onSingleTapUp(MotionEvent e) {
-    return false;
-  }
+	@Override
+	public boolean onDown(MotionEvent e) {
+		return false;
+	}
 
-  @Override
-  public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-    return false;
-  }
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		return false;
+	}
 
-  @Override
-  public void onShowPress(MotionEvent e) {}
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+		return false;
+	}
 
-  @Override
-  public void onLongPress(MotionEvent e) {}
+	@Override
+	public void onShowPress(MotionEvent e) {
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+	}
 }
